edf5ab3e767476b54d72bf25e49720ca40f55c65
npx <harisudheer@lyrostech.com>
2024-06-29 19:58:07 +0530

    commit

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -44,7 +44,7 @@ const Scheduler = () => {
             <tr style={{ height: '50px' }}>
               <td style={{ width: '100%' }}>
                 <DatePickerComponent
-                  value={new Date(2021, 0, 10)}
+                  value={new Date(2024, 0, 10)}
                   showClearButton={false}
                   placeholder="Current Date"
                   floatLabelType="Always"
